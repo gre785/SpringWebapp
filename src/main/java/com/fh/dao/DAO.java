979f3526dcca0a3582fff059a45ab748e957846e
gyang <gre785@gmail.com>
2018-03-19 16:38:28 -0700

    Adding java-doc on DAO

--- src/main/java/com/fh/dao/DAO.java
+++ src/main/java/com/fh/dao/DAO.java
@@ -3,21 +3,59 @@ package com.fh.dao;
 
 public interface DAO
 {
+    /**
+     * @param statement Unique identifier matching the statement to execute.
+     * @param parameter A parameter object to pass to the statement.
+     * @return The number of rows affected by the insert.
+     * @throws Exception
+     */
     public Object save(String statement, Object parameter)
         throws Exception;
 
+    /**
+     * @param statement Unique identifier matching the statement to execute.
+     * @param parameter A parameter object to pass to the statement.
+     * @return The number of rows affected by the update.
+     * @throws Exception
+     */
     public Object update(String statement, Object parameter)
         throws Exception;
 
+    /**
+     * @param statement Unique identifier matching the statement to execute.
+     * @param parameter A parameter object to pass to the statement.
+     * @return The number of rows affected by the delete.
+     * @throws Exception
+     */
     public Object delete(String statement, Object parameter)
         throws Exception;
 
+    /**
+     * @param statement Unique identifier matching the statement to use.
+     * @param parameter A parameter object to pass to the statement.
+     * @return Mapped object
+     * @throws Exception
+     */
     public Object findForObject(String statement, Object parameter)
         throws Exception;
 
+    /**
+     * @param statement Unique identifier matching the statement to use.
+     * @param parameter A parameter object to pass to the statement.
+     * @return List of mapped objects
+     * @throws Exception
+     */
     public Object findForList(String statement, Object parameter)
         throws Exception;
 
+    /**
+     * @param statement Unique identifier matching the statement to use.
+     * @param parameter A parameter object to pass to the statement.
+     * @return Map of mapped objects
+     * @param key
+     * @return
+     * @throws Exception
+     */
     public Object findForMap(String statement, Object parameter, String key)
         throws Exception;
 
