68d0605632b5ede8e941218529b7837792ecc2ee
gyang <gre785@gmail.com>
2018-03-19 17:03:37 -0700

    Refactoring filters

--- src/main/java/com/fh/filter/LoginFilter.java
+++ src/main/java/com/fh/filter/LoginFilter.java
@@ -1,3 +1,4 @@
+
 package com.fh.filter;
 
 import java.io.IOException;
@@ -8,34 +9,27 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import com.fh.controller.base.BaseController;
-import com.fh.util.FileUtil;
 
-/**
- * 登录验证过滤器
- */
-public class LoginFilter extends BaseController implements Filter {
+public class LoginFilter
+    extends BaseController
+    implements Filter
+{
 
-	/**
-	 * 初始化
-	 */
-	public void init(FilterConfig fc) throws ServletException {
-		//FileUtil.createDir("d:/FH/topic/");
-	}
-	
-	public void destroy() {
+    public void init(FilterConfig fc)
+        throws ServletException
+    {
+        // FileUtil.createDir("d:/FH/topic/");
+    }
 
-	}
+    public void destroy()
+    {}
 
-	public void doFilter(ServletRequest req, ServletResponse res,
-			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest request = (HttpServletRequest) req;
-		HttpServletResponse response = (HttpServletResponse) res;
-		
-		chain.doFilter(req, res); // 调用下一过滤器
-	}
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+        throws IOException, ServletException
+    {
+        chain.doFilter(req, res);
+    }
 
 }
