34aac98cf645c6c8fedca3c04aea627d3ca02cfc
gyang <gre785@gmail.com>
2018-03-19 17:17:04 -0700

    Refactoring WebAppContextListener

--- src/main/java/com/fh/listener/WebAppContextListener.java
+++ src/main/java/com/fh/listener/WebAppContextListener.java
@@ -1,3 +1,4 @@
+
 package com.fh.listener;
 
 import javax.servlet.ServletContextEvent;
@@ -6,24 +7,21 @@ import javax.servlet.ServletContextListener;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import com.fh.util.Const;
+
 /**
  * 
-* 类名称：WebAppContextListener.java
-* 类描述： 
-* 作者：FH 
-* 联系方式：
-* @version 1.0
  */
-public class WebAppContextListener implements ServletContextListener {
+public class WebAppContextListener
+    implements ServletContextListener
+{
 
-	public void contextDestroyed(ServletContextEvent event) {
-		// TODO Auto-generated method stub
-	}
+    public void contextDestroyed(ServletContextEvent event)
+    {
+        // TODO Auto-generated method stub
+    }
 
-	public void contextInitialized(ServletContextEvent event) {
-		// TODO Auto-generated method stub
-		Const.WEB_APP_CONTEXT = WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
-		//System.out.println("========获取Spring WebApplicationContext");
-	}
-
+    public void contextInitialized(ServletContextEvent event)
+    {
+        Const.WEB_APP_CONTEXT = WebApplicationContextUtils.getWebApplicationContext(event.getServletContext());
+    }
 }
