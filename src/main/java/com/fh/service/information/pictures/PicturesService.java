b1f48956d4559b6c98194777fb380594f6a1dcad
gyang <gre785@gmail.com>
2018-03-20 17:02:30 -0700

    Refactoring PicturesService

--- src/main/java/com/fh/service/information/pictures/PicturesService.java
+++ src/main/java/com/fh/service/information/pictures/PicturesService.java
@@ -1,3 +1,4 @@
+
 package com.fh.service.information.pictures;
 
 import java.util.List;
@@ -10,75 +11,92 @@ import com.fh.dao.DaoSupport;
 import com.fh.entity.Page;
 import com.fh.util.PageData;
 
-
 @Service("picturesService")
-public class PicturesService {
+public class PicturesService
+{
 
-	@Resource(name = "daoSupport")
-	private DaoSupport dao;
-	
-	/*
-	* 新增
-	*/
-	public void save(PageData pd)throws Exception{
-		dao.save("PicturesMapper.save", pd);
-	}
-	
-	/*
-	* 删除
-	*/
-	public void delete(PageData pd)throws Exception{
-		dao.delete("PicturesMapper.delete", pd);
-	}
-	
-	/*
-	* 修改
-	*/
-	public void edit(PageData pd)throws Exception{
-		dao.update("PicturesMapper.edit", pd);
-	}
-	
-	/*
-	*列表
-	*/
-	public List<PageData> list(Page page)throws Exception{
-		return (List<PageData>)dao.findForList("PicturesMapper.datalistPage", page);
-	}
-	
-	/*
-	*列表(全部)
-	*/
-	public List<PageData> listAll(PageData pd)throws Exception{
-		return (List<PageData>)dao.findForList("PicturesMapper.listAll", pd);
-	}
-	
-	/*
-	* 通过id获取数据
-	*/
-	public PageData findById(PageData pd)throws Exception{
-		return (PageData)dao.findForObject("PicturesMapper.findById", pd);
-	}
-	
-	/*
-	* 批量删除
-	*/
-	public void deleteAll(String[] ArrayDATA_IDS)throws Exception{
-		dao.delete("PicturesMapper.deleteAll", ArrayDATA_IDS);
-	}
-	
-	/*
-	* 批量获取
-	*/
-	public List<PageData> getAllById(String[] ArrayDATA_IDS)throws Exception{
-		return (List<PageData>)dao.findForList("PicturesMapper.getAllById", ArrayDATA_IDS);
-	}
-	
-	/*
-	* 删除图片
-	*/
-	public void delTp(PageData pd)throws Exception{
-		dao.update("PicturesMapper.delTp", pd);
-	}
-	
+    @Resource(name = "daoSupport")
+    private DaoSupport dao;
+
+    /*
+    * add
+    */
+    public void save(PageData pd)
+        throws Exception
+    {
+        dao.save("PicturesMapper.save", pd);
+    }
+
+    /*
+    * delete
+    */
+    public void delete(PageData pd)
+        throws Exception
+    {
+        dao.delete("PicturesMapper.delete", pd);
+    }
+
+    /*
+    * update
+    */
+    public void edit(PageData pd)
+        throws Exception
+    {
+        dao.update("PicturesMapper.edit", pd);
+    }
+
+    /*
+    * find list
+    */
+    public List<PageData> list(Page page)
+        throws Exception
+    {
+        return (List<PageData>)dao.findForList("PicturesMapper.datalistPage", page);
+    }
+
+    /*
+    * find all
+    */
+    public List<PageData> listAll(PageData pd)
+        throws Exception
+    {
+        return (List<PageData>)dao.findForList("PicturesMapper.listAll", pd);
+    }
+
+    /*
+    * find through ID
+    */
+    public PageData findById(PageData pd)
+        throws Exception
+    {
+        return (PageData)dao.findForObject("PicturesMapper.findById", pd);
+    }
+
+    /*
+    *  delete all
+    */
+    public void deleteAll(String[] ArrayDATA_IDS)
+        throws Exception
+    {
+        dao.delete("PicturesMapper.deleteAll", ArrayDATA_IDS);
+    }
+
+    /*
+    * bulk get
+    */
+    public List<PageData> getAllById(String[] ArrayDATA_IDS)
+        throws Exception
+    {
+        return (List<PageData>)dao.findForList("PicturesMapper.getAllById", ArrayDATA_IDS);
+    }
+
+    /*
+    * delete image
+    */
+    public void delTp(PageData pd)
+        throws Exception
+    {
+        dao.update("PicturesMapper.delTp", pd);
+    }
+
 }
-
