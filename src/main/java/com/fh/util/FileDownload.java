1938851e3e195681a5631aa79e686da1bd5c5c86
gyang <gre785@gmail.com>
2018-03-23 16:17:02 -0700

    Refactoring utils

--- src/main/java/com/fh/util/FileDownload.java
+++ src/main/java/com/fh/util/FileDownload.java
@@ -1,3 +1,4 @@
+
 package com.fh.util;
 
 import java.io.BufferedOutputStream;
@@ -6,34 +7,21 @@ import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletResponse;
 
-
-/**
- * 下载文件
- * 创建人：FH 创建时间：2014年12月23日
- * @version
- */
-public class FileDownload {
-
-	/**
-	 * @param response 
-	 * @param filePath		//文件完整路径(包括文件名和扩展名)
-	 * @param fileName		//下载后看到的文件名
-	 * @return  文件名
-	 */
-	public static void fileDownload(final HttpServletResponse response, String filePath, String fileName) throws Exception{  
-		     
-		    byte[] data = FileUtil.toByteArray2(filePath);  
-		    fileName = URLEncoder.encode(fileName, "UTF-8");  
-		    response.reset();  
-		    response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");  
-		    response.addHeader("Content-Length", "" + data.length);  
-		    response.setContentType("application/octet-stream;charset=UTF-8");  
-		    OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());  
-		    outputStream.write(data);  
-		    outputStream.flush();  
-		    outputStream.close();
-		    response.flushBuffer();
-		    
-		} 
-
+public class FileDownload
+{
+    public static void fileDownload(final HttpServletResponse response, String filePath, String fileName)
+        throws Exception
+    {
+        byte[] data = FileUtil.toByteArray2(filePath);
+        fileName = URLEncoder.encode(fileName, "UTF-8");
+        response.reset();
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        response.addHeader("Content-Length", "" + data.length);
+        response.setContentType("application/octet-stream;charset=UTF-8");
+        OutputStream outputStream = new BufferedOutputStream(response.getOutputStream());
+        outputStream.write(data);
+        outputStream.flush();
+        outputStream.close();
+        response.flushBuffer();
+    }
 }
