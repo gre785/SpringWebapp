e86dd4af9b170faff935f5eae619c8b643e0466c
gyang <gre785@gmail.com>
2018-03-30 17:06:41 -0700

    Refactoring Wartermark

--- src/main/java/com/fh/util/Watermark.java
+++ src/main/java/com/fh/util/Watermark.java
@@ -32,70 +32,61 @@ public class Watermark
     {
         if (null != strFWATERM && !"".equals(strFWATERM)) {
             String strFW[] = strFWATERM.split(",fh,");
-            if (strFW.length == 5) {
-                if ("yes".equals(strFW[0])) {
-                    pressText(strFW[1].toString(), imagePath, "", 1, Color.RED, Integer.parseInt(strFW[2]), Integer.parseInt(strFW[3]),
-                        Integer.parseInt(strFW[4]));
-                }
+            if (strFW.length == 5 && "yes".equals(strFW[0])) {
+                pressText(strFW[1].toString(), imagePath, "", 1, Color.RED, Integer.parseInt(strFW[2]), Integer.parseInt(strFW[3]),
+                    Integer.parseInt(strFW[4]));
             }
         }
         if (null != strIWATERM && !"".equals(strIWATERM)) {
             String strIW[] = strIWATERM.split(",fh,");
-            if (strIW.length == 4) {
-                if ("yes".equals(strIW[0])) {
-                    pressImage(PathUtil.getClasspath() + Const.FILEPATHIMG + strIW[1], imagePath, Integer.parseInt(strIW[2]),
-                        Integer.parseInt(strIW[3]));
-                }
+            if (strIW.length == 4 && "yes".equals(strIW[0])) {
+                pressImage(PathUtil.getClasspath() + Const.FILEPATHIMG + strIW[1], imagePath, Integer.parseInt(strIW[2]), Integer.parseInt(strIW[3]));
             }
         }
     }
 
-    public final static void pressImage(String pressImg, String targetImg, int x, int y)
+    public final static void pressImage(String markImageUrl, String targetImageUrl, int x, int y)
     {
         try {
-            File _file = new File(targetImg);
-            Image src = ImageIO.read(_file);
-            int wideth = src.getWidth(null);
-            int height = src.getHeight(null);
-            BufferedImage image = new BufferedImage(wideth, height, BufferedImage.TYPE_INT_RGB);
-            Graphics g = image.createGraphics();
-            g.drawImage(src, 0, 0, wideth, height, null);
+            Image image = ImageIO.read(new File(targetImageUrl));
+            int width = image.getWidth(null);
+            int height = image.getHeight(null);
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics graphics = bufferedImage.createGraphics();
+            graphics.drawImage(image, 0, 0, width, height, null);
 
-            File _filebiao = new File(pressImg);
-            Image src_biao = ImageIO.read(_filebiao);
-            int wideth_biao = src_biao.getWidth(null);
-            int height_biao = src_biao.getHeight(null);
-            g.drawImage(src_biao, x, y, wideth_biao, height_biao, null);
-            g.dispose();
-            FileOutputStream out = new FileOutputStream(targetImg);
+            Image chartImage = ImageIO.read(new File(markImageUrl));
+            graphics.drawImage(chartImage, x, y, chartImage.getWidth(null), chartImage.getHeight(null), null);
+            graphics.dispose();
+            FileOutputStream out = new FileOutputStream(targetImageUrl);
             JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-            encoder.encode(image);
+            encoder.encode(bufferedImage);
             out.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void pressText(String pressText, String targetImg, String fontName, int fontStyle, Color color, int fontSize, int x, int y)
+    public static void pressText(String markImageUrl, String targetImageUrl, String fontName, int fontStyle, Color color, int fontSize, int x, int y)
     {
         try {
-            File _file = new File(targetImg);
-            Image src = ImageIO.read(_file);
-            int wideth = src.getWidth(null);
+            Image src = ImageIO.read(new File(targetImageUrl));
+            int width = src.getWidth(null);
             int height = src.getHeight(null);
-            BufferedImage image = new BufferedImage(wideth, height, BufferedImage.TYPE_INT_RGB);
-            Graphics g = image.createGraphics();
-            g.drawImage(src, 0, 0, wideth, height, null);
-            g.setColor(color);
-            g.setFont(new Font(fontName, fontStyle, fontSize));
-            g.drawString(pressText, x, y);
-            g.dispose();
-            FileOutputStream out = new FileOutputStream(targetImg);
+            BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            Graphics graphics = bufferedImage.createGraphics();
+
+            graphics.drawImage(src, 0, 0, width, height, null);
+            graphics.setColor(color);
+            graphics.setFont(new Font(fontName, fontStyle, fontSize));
+            graphics.drawString(markImageUrl, x, y);
+            graphics.dispose();
+            FileOutputStream out = new FileOutputStream(targetImageUrl);
             JPEGImageEncoder encoder = JPEGCodec.createJPEGEncoder(out);
-            encoder.encode(image);
+            encoder.encode(bufferedImage);
             out.close();
         } catch (Exception e) {
-            System.out.println(e);
+            e.printStackTrace();
         }
     }
 }
